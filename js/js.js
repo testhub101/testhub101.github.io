1bf43624739cc8cb6221c7fc93910a3b36ea685f
Jonathon Temelkovski <jonathon.temelkovski@mail.utoronto.ca>
2018-05-26 12:36:33 -0400

    test

---
+++ js/js.js
@@ -0,0 +1,11 @@
+$(function(){
+    $(".name").typed({
+        strings: [
+            "onathan",
+            "onathon Temelkovski"
+        ],
+        typeSpeed: 30,
+        backSpeed: 40,
+        backDelay: 300
+    });
+});
