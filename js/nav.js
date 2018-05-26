7d55abfb75cd0ac47d1dfeeeb9954172ef6c0874
Jonathon Temelkovski <jonathon.temelkovski@mail.utoronto.ca>
2018-05-26 13:19:07 -0400

    fixed js

--- js/nav.js
+++ js/nav.js
@@ -3,9 +3,7 @@ $(document).ready(function(){
     var startProductBarPos=-1;
     window.onscroll=function(){
         var bar = document.getElementById('nav');
-        if (bar == null) {
-            document.body.innerHTML = '';
-        }
+        
         if(startProductBarPos<0)startProductBarPos=findPosY(bar);
 
         if(pageYOffset>startProductBarPos){
